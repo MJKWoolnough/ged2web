552f82d4616571253e63697168dad73626a358d0
Michael Woolnough <michael.woolnough@gmail.com>
2021-07-07 13:45:13 +0100

    use optional chaning to avoid errors

--- ged2web.ts
+++ ged2web.ts
@@ -4,7 +4,7 @@ declare const pageLoad: Promise<void>;
 
 pageLoad.then(() => {
 	const base = document.getElementById("ged2web") || document.body;
-	switch (window.location.pathname.split("/").pop()!.split(".").shift()) {
+	switch (window.location.pathname.split("/").pop()?.split(".").shift()) {
 	default:
 		list(base);
 	}
