03bb7108e07fee14e7b2f3102e74462b07661f83
Michael Woolnough <michael.woolnough@gmail.com>
2021-07-15 08:36:59 +0100

    need to actually return container

--- tree.ts
+++ tree.ts
@@ -119,5 +119,5 @@ class Spouse extends PersonBox {
 }
 
 export default function({idStr, highlightStr}: Record<string, string | number>) {
-	new Tree(typeof idStr === "string" ? parseInt(idStr) : idStr, (highlightStr as string).split(",").map(id => parseInt(id)).filter(id => id > 0)).container;
+	return new Tree(typeof idStr === "string" ? parseInt(idStr) : idStr, (highlightStr as string).split(",").map(id => parseInt(id)).filter(id => id > 0)).container;
 }
