941f707f79815bf53c8e3971b3f531f2fdd4f0a4
Michael Woolnough <michael.woolnough@gmail.com>
2021-09-15 13:57:23 +0100

    corrected embedding

--- embed.sh
+++ embed.sh
@@ -20,7 +20,7 @@ fi;
 
 	echo -n "	jsStart   = \"";
 
-	jslib -i /ged2web.js -n -x | tail -n+2 | sed -e 's/pageLoad/(document.readyState == "complete" ? Promise.resolve() : new Promise(successFn => globalThis.addEventListener("load", successFn, {once: true})))/' | terser -m  --module --compress pure_getters,passes=3 --ecma 6 | tr -d '\n' | sed -e 's/\\/\\\\/g' -e 's/"/\\\"/g' -e 's/\(.*=\[\)\(\],[^=]*=\[\)/\1\"'"\n"'	jsMid     = \"\2\"'"\n"'	jsEnd     = \"/';
+	jslib -i /ged2web.js -n -x | tail -n+2 | sed -e 's/pageLoad/(document.readyState == "complete" ? Promise.resolve() : new Promise(successFn => globalThis.addEventListener("load", successFn, {once: true})))/' | terser -m  --module --compress pure_getters,passes=3 --ecma 6 | tr -d '\n' | sed -e 's/\\/\\\\/g' -e 's/"/\\\"/g' -e 's/\(.*=\[\)\(\],[^=]*=\[\)\]/\1\"'"\n"'	jsMid     = \"\2\"'"\n"'	jsEnd     = \"\]/';
 
 	echo "\"";
 
