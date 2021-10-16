d83ba4b602413b3e74adb1080ff98237ba272862
Michael Woolnough <michael.woolnough@gmail.com>
2021-10-16 11:13:54 +0100

    more strict with gender type

--- gedcom.d.ts
+++ gedcom.d.ts
@@ -1,1 +1,1 @@
-export const people: [string | undefined, string | undefined, string | undefined, string | undefined, number, number, ...number[]][], families: [number, number, ...number[]][];
+export const people: [string | undefined, string | undefined, string | undefined, string | undefined, 0 | 1 | 2, number, ...number[]][], families: [number, number, ...number[]][];
