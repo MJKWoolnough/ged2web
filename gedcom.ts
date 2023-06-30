37b50a15b0a53afd4e0888107e6e7228639c0057
Michael Woolnough <michael.woolnough@gmail.com>
2023-06-30 18:43:19 +0100

    replaced typescript definition file with empty gedcom data in a typescript file

---
+++ gedcom.ts
@@ -0,0 +1,1 @@
+export const people: [string | undefined, string | undefined, string | undefined, string | undefined, 0 | 1 | 2, number, ...number[]][] = [], families: [number, number, ...number[]][] = [];
