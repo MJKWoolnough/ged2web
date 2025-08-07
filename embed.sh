1ca5f8339e60dfc24755d3d3398d48204d8b1ed8
Michael Woolnough <michael.woolnough@gmail.com>
2025-08-07 07:13:22 +0100

    formatting

--- embed.sh
+++ embed.sh
@@ -14,7 +14,7 @@ fi;
 	echo "const (";
 	echo "	htmlStart = \"<html lang=\\\"en\\\"><head><title>Ged2Web</title><meta charset=\\\"UTF-8\\\" /><script type=\\\"module\\\">\"";
 	echo -n "	htmlEnd   = \"</script><style type=\\\"text/css\\\">";
-	uglifycss style.css | sed -e 's/\"/\\\"/g' | tr -d "\n"; 
+	uglifycss style.css | sed -e 's/\"/\\\"/g' | tr -d "\n";
 	echo "</style></head><body></body></html>\"";
 	echo "	modStart  = \"export const people = [\"";
 	echo "	modMid    = \"], families = [\"";
@@ -27,4 +27,4 @@ fi;
 	echo "\"";
 
 	echo ")";
-) > parts.go
+) > parts.go;
